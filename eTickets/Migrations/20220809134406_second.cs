09fcb0ceabd9017055eec668aa59567a23907548
U180709035 <ahmtdavut@gmail.com>
2022-08-15 10:54:55 +0300

    first

---
+++ eTickets/Migrations/20220809134406_second.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURl",
+                table: "Movies",
+                newName: "ImageURL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURL",
+                table: "Movies",
+                newName: "ImageURl");
+        }
+    }
+}
